6cc02b6cde0dfd463fd5b2079c3e856276114da7
Tiberius <tiberius.santos@yahoo.com>
2022-04-23 19:22:27 -0300

    Commit inicial

---
+++ sw.js
@@ -0,0 +1,39 @@
+const storage = "delivery";
+const content = [
+  "/",
+  "/index.html",
+  "/static/js/app.js",
+  "/static/css/style.css",
+  "/static/img/favicon.png",
+  "/static/img/calc.svg",
+  "/static/img/logo.svg",
+  "/static/img/whatsapp.svg",
+  "/static/img/icons/icon-192.png",
+  "/static/img/icons/icon-384.png",
+  "/static/img/icons/icon-512.png",
+];
+
+// Install the serviceWorker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(storage);
+      await cache.addAll(content);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      // Return the request if already in the cache
+      const request = await caches.match(e.request);
+      if (request) return request;
+      // Cache and return the updated request
+      const response = await fetch(e.request);
+      const cache = await caches.open(storage);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
